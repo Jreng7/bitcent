0c896fe75462dd129863ff711e08bfbb467773ec
waydev <jr.waydev@gmail.com>
2023-06-22 10:56:57 -0300

    feat: add etc..

--- src/app.js
+++ src/app.js
@@ -2,9 +2,20 @@ import express from 'express';
 
 const app = express();
 
+
+const selecoes = [
+  {id: 1, pais: 'brasil', grupo: 'A'},
+  {id: 2, pais: 'EUA', grupo: 'B'}
+]
+
 // Criar rota padrão ou raiz.
 app.get('/', (req, res) => {
-  res.send('Olá')
+  res.send('Curso de Node.JS')
+})
+
+// Lista seleção.
+app.get('/selecoes', (req, res) => {
+  res.send(selecoes)
 })
 
 export default app;
