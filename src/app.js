e982b4265315460422031f376cb97af16cdc666f
waydev <jr.waydev@gmail.com>
2023-06-22 10:08:44 -0300

    feat: rota padrão ou raiz.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import express from 'express';
 
-const app = express();
+const app = expre
 
+// Criar rota padrão ou raiz.
 app.get('/', (req, res) => {
-
+  res.send('Olá')
 })
 
 export default app;
